f0729a43bf757c764ceb2d410f8af531a93862ef
Eric Schles <ericschles@gmail.com>
2015-09-11 21:38:10 -0400

    things

--- functions_variables_lists_dictionaries.py
+++ functions_variables_lists_dictionaries.py
@@ -1,3 +1,7 @@
+#By convention we keep all our imports at the top of the file, but you can put them elsewhere (please don't)
+import json
+import requests
+import lxml.html
 
 def remove_all(listing,x):
     while x in listing:
@@ -35,3 +39,36 @@ print listing
 list_two = [1,1,4,3,4,1,5]
 print "This is our original list",list_two
 print "This is our list after removing all the 1s",remove_all(list_two,1)
+
+dictionary = {}
+dictionary["Eric"] = "Eric is 6', he is 29, and he likes everyone!"
+print dictionary
+print json.dumps(dictionary) #converted to json, what up
+
+#the messy way
+text = requests.get("https://www.google.com").text
+html = lxml.html.fromstring(text)
+links = html.xpath("//a/@href")
+safe_links = []
+for link in links:
+    if link.startswith("http"):
+        safe_links.append(link)
+
+responses = []
+for link in safe_links:
+    responses.append(requests.get(link))
+print responses
+
+#the clean way
+text = requests.get("https://www.google.com").text
+html = lxml.html.fromstring(text)
+links = html.xpath("//a/@href")
+responses = []
+for link in links:
+    print link
+    if link.startswith("http"):
+        responses.append(requests.get(link))
+print responses
+
+
+
