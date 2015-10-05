3a5f1cd18b57e778862e9878a3d15bee5846d84b
Lauren Gibbons <laurengibbons@lgibbons.home>
2015-10-04 20:34:00 -0400

    PHASE 2

--- lesson_two/one.py
+++ lesson_two/one.py
@@ -1,3 +1,9 @@
+import pickle
+
 name = str(raw_input("Hello there, what's your name?")) 
 occupation = str(raw_input("Where do you work?"))
 print name,occupation
+dict = {}
+dict["name"] = name
+dict["occupation"] = occupation
+pickle.dump(dict,open("user.pickle","w"))
